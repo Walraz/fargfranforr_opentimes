85dbf749cf21e97c25da47295a3795a925d2efc3
Walraz <hallstrom.rasmus@gmail.com>
2017-03-06 19:50:00 +0100

    cors

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var mysql = require('mysql');
 var express = require('express');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 var app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 
 var config = {
